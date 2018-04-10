7a6da09d85735b06b8dcc354d8cc20e57daefc79
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-04-10 13:38:49 +0900

    routesの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+
+  root :to => 'users#index'
+
 end
+
+# 記事上
+# CloudinaryDemo::Application.routes.draw do
+#   resources :users
+
+#   root :to => 'users#index'
+# end
