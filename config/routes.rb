31d56ca9287b363b4b0fbca2c7d1cba90b5b6ee8
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-04-10 14:03:48 +0900

    routesの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users
 
+  # 既存の状態に下記１文のみを追加
   root :to => 'users#index'
-
 end
 
 # 記事上
